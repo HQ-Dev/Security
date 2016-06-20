febe7d7ca4d8b2a51c3397aadefae3a5f5a7f273
archy <hq_dev@163.com>
2016-06-21 01:55:38 +0800

    完成Base64 加密算法的实践

---
+++ src/main/java/com/security/base/Base64.java
@@ -0,0 +1,41 @@
+package main.java.com.security.base;
+
+
+public class Base64 {
+
+	// 将要加密的原始字符串
+	private static String src = "security jdkBase 64";
+	
+	
+	public static void main(String[] args) {
+		System.out.println("commonsCodesBase64():");
+		commonsCodesBase64();
+		System.out.println("bouncyCastleBase64():");
+		bouncyCastleBase64();
+		
+	}
+	
+	// Jdk 默认方法加密
+	public static void jdkBase64() {
+		
+	}
+	
+	// 使用 Commons Codec 
+	public static void commonsCodesBase64() {
+		byte[] encodeBytes = org.apache.commons.codec.binary.Base64.encodeBase64(src.getBytes());
+		System.out.println("encode:" + new String(encodeBytes));
+		
+		byte[] decodeBytes = org.apache.commons.codec.binary.Base64.decodeBase64(encodeBytes);
+		System.out.println("decode:" + new String(decodeBytes));
+	}
+	
+	// 使用 Bouncy Castle
+	public static void bouncyCastleBase64() {
+		byte[] encodeBytes = org.bouncycastle.util.encoders.Base64.encode(src.getBytes());
+		System.out.println("encode:" + new String(encodeBytes));
+		
+		byte[] decodeBytes  = org.bouncycastle.util.encoders.Base64.decode(encodeBytes);
+		System.out.println("decode:" + new String(decodeBytes));
+	}
+
+}
